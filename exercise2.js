bd6532d17cd0e992ca21a28c66936b15cebdf3e0
Emmanuel <emmsom506@gmail.com>
2025-12-29 19:59:16 +0000

    Complete exercise 2

---
+++ exercise2.js
@@ -0,0 +1,51 @@
+// Solution for Question6
+const score = 75;
+
+const solutionNo = "Question6 answer =>"
+
+if (score >= 50) {
+  console.log(`${solutionNo} Pass`);
+} else {
+  console.log(`${solutionNo} Fail`);
+}
+
+// Solution for Question7
+const a = 12;
+const b = 12;
+
+const questionNo = "Question7 answer =>";
+
+if (a > b) {
+  console.log(`${questionNo} ${a} is greater than ${b}`);
+} else if (a < b) {
+  console.log(`${questionNo} ${a} is less than ${b}`);
+} else {
+  console.log(`${questionNo} ${a} is equal to ${b}`);
+}
+
+// Solution for Question8
+const age = 16
+const answerNo = "Question8 answer =>"
+if(age >= 18) {
+  console.log(`${answerNo} Can vote`);
+} else {
+  console.log(`${answerNo} Cannot vote`);
+}
+
+// Solution for Question9
+const isWeekend = true;
+const answerN09 = "Question9 answer =>"
+if(isWeekend) {
+  console.log(`${answerN09} No work today!`);
+} else {
+  console.log(`${answerN09} Time to work!`);
+}
+
+// Solution for Question10
+const num = 24
+const answerNo10 = "Question10 answer =>"
+if(num % 2 === 0 ) {
+  console.log(`${answerNo10} ${num} is even number`)
+} else {
+  console.log(`${answerNo10} ${num} is old number`)
+}
